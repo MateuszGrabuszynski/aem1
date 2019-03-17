639808c893ad01c0c06fea2e82a1e08b00fbbd9c
mgrabuszynski <mgrabuszynski@gmail.com>
2019-03-17 13:23:17 +0100

    1st try grouping

--- read_data.py
+++ read_data.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-import collections
 
 from scipy.spatial.distance import pdist, squareform
-from scipy import mean
 from sklearn.cluster import KMeans
 
 
@@ -22,13 +19,12 @@ labels_cpy = labels.copy()
 points = pd.DataFrame(xys, columns=columns, index=labels)
 
 dists_orig = pd.DataFrame(squareform(pdist(points, metric='euclidean')), columns=labels, index=labels)
-# print(dists_orig.head(10))
 
 dists = dists_orig.copy()
 # dists.to_csv('distances.csv')
 
 km = KMeans(n_clusters=10,
-            init='k-means++',
+            init='random',
             n_init=10,
             max_iter=300,
             tol=1e-04,
@@ -36,24 +32,42 @@ km = KMeans(n_clusters=10,
 
 y_km = km.fit_predict(points)
 
+
 x = []
 y = []
 clrs = []
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray',
           'tab:olive', 'tab:cyan']
 
-for i in range(len(points)):
-    x.append(points.iloc[i][0])
-    y.append(points.iloc[i][1])
-    clrs.append(colors[y_km[i]])
+# for i in range(len(points)):
+#     x.append(points.iloc[i][0])
+#     y.append(points.iloc[i][1])
+#     clrs.append(colors[y_km[i]])
+#
+# plt.scatter(
+#     x,
+#     y,
+#     c=clrs
+# )
+#
+# plt.xlabel('X')
+# plt.ylabel('Y')
+#
+# plt.show()
 
-plt.scatter(
-    x,
-    y,
-    c=clrs
-)
 
-plt.xlabel('X')
-plt.ylabel('Y')
+pts = pd.DataFrame({
+    'point': points.index,
+    'x': points['X'],
+    'y': points['Y'],
+    'gr': y_km
+})
+print(pts)
 
-plt.show()
+gr_dists = []
+for i in range(10):
+    gr_points = pts.loc[pts['gr'] == i]['point']
+    # print(gr_points.values.tolist())
+    gr_dists[i] = dists_orig.loc[gr_points.values.tolist()]
+    print(i)
+    print(gr_dists[i])
